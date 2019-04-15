4377041c68fdcac5e1b687a2e2f8b847358864f3
Moises <musicmojica@gmail.com>
2019-04-15 07:02:51 -0600

    Complete perfil

--- app/src/main/java/com/moisesmojica/myaventon/activities/PerfilActivity.java
+++ app/src/main/java/com/moisesmojica/myaventon/activities/PerfilActivity.java
@@ -1,16 +1,31 @@
 package com.moisesmojica.myaventon.activities;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import com.moisesmojica.myaventon.R;
+import com.moisesmojica.myaventon.api.Api;
+import com.moisesmojica.myaventon.api.RequestHandler;
+import com.moisesmojica.myaventon.helper.Constant;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class PerfilActivity extends MainActivity {
+    private TextView editTextNombre, editTextApeellido, editTextEmail, editTextCelular;
+    private ImageView imageViewPefil;
 
 
 
@@ -24,6 +39,25 @@ public class PerfilActivity extends MainActivity {
         View contentView = inflater.inflate(R.layout.activity_perfil, null, false);
         drawerLayout.addView(contentView, 0);
         navigationView.setCheckedItem(R.id.menu_perfil);
+
+        bindUI();
+    }
+
+    private void bindUI() {
+        editTextNombre = findViewById(R.id.textViewPerfilNombre);
+        editTextApeellido = findViewById(R.id.textViewPerfilApellido);
+        editTextEmail = findViewById(R.id.textViewPerfilCorreo);
+        editTextCelular =  findViewById(R.id.textViewPerfilCelular);
+        imageViewPefil = findViewById(R.id.imageViewPerfilPicture);
+
+        editTextNombre.setText(Constant.USER_NOMBRE);
+        editTextApeellido.setText(Constant.USER_APELLIDO);
+        editTextEmail.setText(Constant.USER_EMAIL);
+        editTextCelular.setText(Constant.USER_CELULAR);
+        Picasso.get()
+                .load(Api.URL_IMAGE)
+                .resize(100, 100)
+                .into(imageViewPefil);
 
     }
 
@@ -46,5 +80,49 @@ public class PerfilActivity extends MainActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+
+
+        String url;
+        HashMap<String, String> params;
+
+        int requestCode;
+
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode){
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            super.onPostExecute(s);
+            try {
+                JSONObject object = new JSONObject(s);
+                if(object.getInt("error")== 0 ){
+                  //  refreshContenidoList(object.getJSONArray("result"));
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            RequestHandler requestHandler = new RequestHandler();
+            if(requestCode == Api.CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+            if ((requestCode == Api.CODE_GET_REQUEST))
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/moisesmojica/myaventon/helper/Constant.java
@@ -0,0 +1,10 @@
+package com.moisesmojica.myaventon.helper;
+
+public  class Constant {
+
+    public static   final String USER_NOMBRE = "CARLOS ANDRES";
+    public static final String USER_APELLIDO = "RAMOS ORTEGA";
+    public static final String USER_EMAIL = "carlos.ortega@gmail.com";
+    public  static final String USER_CELULAR = "89166565";
+
+}
